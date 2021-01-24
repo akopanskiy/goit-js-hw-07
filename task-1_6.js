4a1e6795bea26b96a1bb5aee8e18f9b444c53178
Andrii Kopanskiy <andriikopanskiy@gmail.com>
2021-01-24 23:00:07 +0200

    update task 6 i 7

--- task-1_6.js
+++ task-1_6.js
@@ -3,7 +3,7 @@ console.dir(inputRef);
 
 inputRef.addEventListener('blur', event => {
   if (event.target.value.length == inputRef.dataset.length) {
-    return inputRef.setAttribute('class', 'valid');
+    inputRef.setAttribute('class', 'valid');
   }
-  return inputRef.setAttribute('class', 'invalid');
+  inputRef.setAttribute('class', 'invalid');
 });
--- task-1_7.js
+++ task-1_7.js
@@ -3,5 +3,5 @@ const spanRef = document.querySelector('#text');
 
 inputRef.addEventListener('input', event => {
   const valueInput = event.target.value;
-  return (spanRef.style.fontSize = `${valueInput}px`);
+  spanRef.style.fontSize = `${valueInput}px`;
 });
